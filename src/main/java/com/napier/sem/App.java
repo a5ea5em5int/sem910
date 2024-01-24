78846b30d799d920eaffee59dbe30e94cfc15546
KhinHsuYatiHtet <khinhsuyatihtet@gmail.com>
2024-01-24 20:57:43 +0630

    feature 1 with greet method is added

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,6 +1,10 @@
 package com.napier.sem;
 
 public class App {
+    public void greet()
+    {
+        System.out.println("Hello greet");
+    }
     public static void main(String[] args) {
         System.out.println("Boo Yah!");
     }
